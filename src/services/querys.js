c094231c2bb8845e37e645afd935a9f1cd5c7bef
crhistian garcia <garcia.crhistian@correounvalle.edu.co>
2022-03-03 03:02:07 +0100

    Initial structure and querys

---
+++ src/services/querys.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+export const getCoordinates = async (location) => {
+  const res = await axios.get(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=dc95562776b6b4eaf655bac72e985edb`.trim()
+  );
+  return res;
+};
+
+export const getCurrentWeather = async (lat, lon) => {
+  try {
+    const res = axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,daily&appid=dc95562776b6b4eaf655bac72e985edb`.trim()
+    );
+    const { timezone_offset } = res;
+    const { dt, temp, feels_like, weather } = res.current;
+    const localTime = parseInt(timezone_offset) + parseInt(dt);
+
+    return { localTime, temp, feels_like, weather };
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getNext48Hours = async (lat, lon) => {
+  try {
+    const res = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,current,daily&appid=dc95562776b6b4eaf655bac72e985edb`.trim()
+    );
+
+    const { hourly, timezone_offset } = res;
+    if (hourly.length !== 0) return { hourly, timezone_offset };
+    return console.warn('No data for this location');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getNextWeek = async (lat, lon) => {
+  try {
+    const res = axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,current,hourly&appid=dc95562776b6b4eaf655bac72e985edb`.trim()
+    );
+
+    const { daily, timezone_offset } = res;
+    if (daily.length !== 0) return { daily, timezone_offset };
+    return console.warn('No data for this location');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getIcon = async (iconId) => {
+  return <img src={`http://openweathermap.org/img/wn/${iconId}.png`} alt='weather.png' />;
+};
